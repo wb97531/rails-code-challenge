6052642ce38ea02d1a4544a87dcc99c848fbe4c5
John Gray <wopr42@gmail.com>
2018-10-23 10:20:01 -0600

    Use presence with booleans in a hash
    The tests cover setting a value in the hash to true, which works fine.
    Setting a value to false will leave it unset (nil) because of the way
    `presence` and `present?` work.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,23 @@
 class Order < ApplicationRecord
+  def expedited?
+    @expedite
+  end
+
+  def returnable?
+    @returns
+  end
+
+  def settings(opts = {})
+    @expedite = opts[:expedite].presence
+    @returns = opts[:returns].presence
+    @warehouse = opts[:warehouse].presence
+  end
+
   def shipped?
     shipped_at.present?
+  end
+
+  def warehoused?
+    @warehouse
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,14 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it { is_expected.to respond_to(:shipped?) }
+  describe '#shipped?' do
+    it { is_expected.to respond_to(:shipped?) }
 
-  context 'when a shipped date exists' do
-    before { subject.update(shipped_at: Time.now) }
-    it { is_expected.to be_shipped }
+    context 'when a shipped date exists' do
+      before { subject.update(shipped_at: Time.now) }
+      it { is_expected.to be_shipped }
+    end
+
+    context 'when no shipped date is present' do
+      it { is_expected.to_not be_shipped }
+    end
   end
 
-  context 'when no shipped date is present' do
-    it { is_expected.to_not be_shipped }
+  describe '#settings' do
+    it { is_expected.to respond_to(:settings) }
+
+    context 'when expedite is present' do
+      before { subject.settings(expedite: true) }
+      it { is_expected.to be_expedited }
+    end
+
+    context 'when returns is present' do
+      before { subject.settings(returns: true) }
+      it { is_expected.to be_returnable }
+    end
+
+    context 'when warehouse is present' do
+      before { subject.settings(warehouse: true) }
+      it { is_expected.to be_warehoused }
+    end
   end
 end
