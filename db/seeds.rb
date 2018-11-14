95a564d8d799696a0e6b66960cb363d5f4f835ff
Wendy Brannon <wb9753@gmail.com>
2018-11-14 15:39:56 -0700

    fixed by remove the 'minute' NoMethod error
      * removed duplication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
-# Creating non-shipped orders.
-o1 = Order.create!()
-o2 = Order.create!()
-o3 = Order.create!()
-o4 = Order.create!()
-o5 = Order.create!()
-# Shipped at times are randomly assigned.  What great randomizaiton I've done!
-os1 = Order.create!(shipped_at: Time.now + 21.minute)
-os2 = Order.create!(shipped_at: Time.now + 20.minute)
-os3 = Order.create!(shipped_at: Time.now + 3.minute)
-os4 = Order.create!(shipped_at: Time.now + 40.minute)
-os5 = Order.create!(shipped_at: Time.now + 5.minute)
+@ship_times = []
+
+5.times {
+  @ship_times += [nil]
+  @ship_times += [Time.now + rand(0..99) * 60]
+}
+
+10.times { Order.create!(shipped_at: @ship_times.pop) }
