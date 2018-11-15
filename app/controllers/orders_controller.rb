7584473f64e889157fc577480de7ad23f8eae019
Wendy Brannon <wb9753@gmail.com>
2018-11-14 17:00:09 -0700

    group orders by ship status and ordering by ship date
      * also displaying ship time to show order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   def index
     @orders = Order.all
+    @shipped_orders = @orders.select {|o| o.shipped?}
+    @non_shipped_orders = @orders.select {|o| !o.shipped?}
   end
 
   def show
